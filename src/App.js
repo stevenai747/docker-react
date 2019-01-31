8d30b335754ee7a0c81941b5b2c0ff949837a9f7
steven <stevenai.mm@gmail.com>
2019-01-31 16:52:15 +0800

    Change in feature branch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            XXX Edit <code>src/App.js</code> and save to reload.
+            This is change in feat
           </p>
           <a
             className="App-link"
